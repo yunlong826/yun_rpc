7e2694a12c32dff2b095cc1f2930870ff963043f
tom_glb <goodmorning_glb@atguigu.com>
2022-05-21 14:48:21 +0800

    初次提交

---
+++ rpc-core/src/main/java/com/yun/rpc/core/balancer/LoadBalancer.java
@@ -0,0 +1,10 @@
+package com.yun.rpc.core.balancer;
+
+/**
+ * @author jack_yun
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/5/21 13:31
+ */
+public interface LoadBalancer {
+}
---
+++ rpc-core/src/test/java/com/yun/rpc/core/RpcCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yun.rpc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
